701ccdf3ba5153d29897400218d6be46be05a59e
Chris Agostino <cjp.agostino@gmail.com>
2026-08-25 22:00:15 -0500

    ver

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ extra_files = package_files("npcpy/npc_team/")
 
 setup(
     name="npcpy",
-    version="2.1.14",
+    version="2.1.16",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  
     extras_require={
